b9458cebd86d3afefd49224876b5556bf722072e
Slezak Attila <attila.slezak@gmail.com>
2016-05-03 01:20:40 +0100

    Encoding task is added

---
+++ Encoding/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\testfiles\boot.txt");
+            StreamWriter sw = new StreamWriter(@"C:\testfiles\boot-utf7.txt", false, System.Text.Encoding.UTF7);
+
+            sw.WriteLine(sr.ReadToEnd());
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
